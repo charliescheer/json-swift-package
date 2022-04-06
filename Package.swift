4bcf15b9d3e9de27b1714e6fe96b17adb95e690c
Charlie Scheer <charlie.scheer@automattic.com>
2022-04-06 15:14:01 +0100

    Removed testing target

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "DayOneMocks",
             dependencies: []),
-        .testTarget(
-            name: "DayOneMocksTests",
-            dependencies: ["DayOneMocks"]),
     ]
 )
